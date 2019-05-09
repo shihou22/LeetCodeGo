ddf4396fcd076a07f5d3c935764cac6e2984b640
shihou22 <pc9801bx2@gmail.com>
2019-05-09 14:58:16 +0900

    practiceFunction Key 10

--- src/easy/Remove Duplicates from Sorted List/main.go
+++ src/easy/Remove Duplicates from Sorted List/main.go
@@ -3,19 +3,19 @@ package main
 func main() {
 
 }
-func climbStairs(n int) int {
-	var dp = make([]int, n+2)
-	dp[0] = 1
-	for index := 0; index < n; index++ {
-		dp[index+1] = dp[index] + dp[index+1]
-		dp[index+2] = dp[index] + dp[index+2]
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
 	}
-	// dp[0] = 1
-	// dp[1] = 1
-	// for index := 2; index <= n; index++ {
-	// 	dp[index] = dp[index-1] + dp[index-2]
-	// }
-	return dp[n]
+	if head.Val == head.Next.Val {
+		head.Next = head.Next.Next
+		deleteDuplicates(head)
+	} else {
+		deleteDuplicates(head.Next)
+	}
+
+	return head
 }
 
 /*
---
+++ src/easy/Remove Duplicates from Sorted List/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_deleteDuplicates(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{name: "1", args: args{&ListNode{1, &ListNode{1, &ListNode{2, nil}}}}, want: &ListNode{1, &ListNode{2, nil}}},
+		{name: "2", args: args{&ListNode{1, &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{3, nil}}}}}}, want: &ListNode{1, &ListNode{2, &ListNode{3, nil}}}},
+		{name: "3", args: args{&ListNode{1, &ListNode{1, &ListNode{1, nil}}}}, want: &ListNode{1, nil}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
