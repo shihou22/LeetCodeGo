ac2f34b665009cce257788c39dca4c8ab45072f3
shihou22 <pc9801bx2@gmail.com>
2019-05-11 12:32:29 +0900

    practice

---
+++ src/easy/Best Time to Buy and Sell Stock II/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "1", args: args{[]int{7, 1, 5, 3, 6, 4}}, want: 7},
+		{name: "2", args: args{[]int{1, 2, 3, 4, 5}}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ src/easy/Pascal's Triangle/main.go
@@ -0,0 +1,105 @@
+package main
+
+func main() {
+
+}
+func generate(numRows int) [][]int {
+	if numRows == 0 {
+		return nil
+	}
+	var res = make([][]int, 0)
+	for index := 0; index < numRows; index++ {
+		var tmp = make([]int, 0)
+		for inner := 0; inner < index+1; inner++ {
+			if inner == 0 || inner == index {
+				tmp = append(tmp, 1)
+			} else {
+				tmp = append(tmp, res[index-1][inner-1]+res[index-1][inner])
+			}
+		}
+		res = append(res, tmp)
+	}
+	return res
+}
+
+/*
+TreeNode
+*/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/*
+ListNode
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//Reverse string reverse
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+//SampleStruct privateで構造体宣言
+type SampleStruct struct {
+	Index  int
+	Height int
+}
+
+/*
+sort用
+GO 1.6ではsort#Sliceが使えないのでこちらで対応
+*/
+type samples []SampleStruct
+
+func (u samples) Len() int {
+	return len(u)
+}
+
+func (u samples) Less(i, j int) bool {
+	return u[i].Height > u[j].Height
+}
+
+func (u samples) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+//Int64Abs abstract int64
+func Int64Abs(a int64) int64 {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
+//IntAbs abstract int
+func IntAbs(a int) int {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
+//IntMin return minimum value
+func IntMin(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+//IntMax return maximum value
+func IntMax(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
