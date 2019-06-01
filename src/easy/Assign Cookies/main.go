4cea9752fc07b041132878d40101e621ba55e5fa
shihou22 <pc9801bx2@gmail.com>
2019-06-01 11:56:33 +0900

    practice

---
+++ src/easy/Assign Cookies/main.go
@@ -0,0 +1,113 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+
+func findContentChildren(g []int, s []int) int {
+
+	sort.Sort(sort.IntSlice(g))
+	sort.Sort(sort.IntSlice(s))
+
+	res := 0
+	left := 0
+	right := 0
+	for left < len(g) {
+
+		for right < len(s) && s[right] < g[left] {
+			right++
+		}
+		if right >= len(s) {
+			break
+		} else {
+			res++
+			right++
+		}
+		left++
+	}
+	return res
+}
+
+/*
+TreeNode
+*/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/*
+ListNode
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//Reverse string reverse
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+//SampleStruct privateで構造体宣言
+type SampleStruct struct {
+	Index  int
+	Height int
+}
+
+/*
+sort用
+GO 1.6ではsort#Sliceが使えないのでこちらで対応
+*/
+type samples []SampleStruct
+
+func (u samples) Len() int {
+	return len(u)
+}
+
+func (u samples) Less(i, j int) bool {
+	return u[i].Height > u[j].Height
+}
+
+func (u samples) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+//Int64Abs abstract int64
+func Int64Abs(a int64) int64 {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
+//IntAbs abstract int
+func IntAbs(a int) int {
+	if a < 0 {
+		a *= -1
+	}
+	return a
+}
+
+//IntMin return minimum value
+func IntMin(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+//IntMax return maximum value
+func IntMax(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
